21d93c0c6277d391ad80ddbc3effc9337d9541fe
saksit malai <saksitma1ai@gmail.com>
2019-10-03 13:52:25 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Translatesub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelcut = new System.Windows.Forms.Panel();
+            this.panelcolor = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.picDataGrid = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.txtInputFile);
+            this.panel1.Controls.Add(this.browse);
+            this.panel1.Location = new System.Drawing.Point(-7, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1092, 33);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInputFile.Location = new System.Drawing.Point(84, 5);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(363, 26);
+            this.txtInputFile.TabIndex = 1;
+            this.txtInputFile.Visible = false;
+            // 
+            // browse
+            // 
+            this.browse.BackColor = System.Drawing.Color.Black;
+            this.browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browse.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse.ForeColor = System.Drawing.Color.Yellow;
+            this.browse.Location = new System.Drawing.Point(6, 0);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 33);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = false;
+            this.browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.panelcut);
+            this.groupBox1.Controls.Add(this.panelcolor);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(529, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(533, 511);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(70, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "ตกลง";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panelcut
+            // 
+            this.panelcut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelcut.BackColor = System.Drawing.Color.Yellow;
+            this.panelcut.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelcut.Location = new System.Drawing.Point(63, 399);
+            this.panelcut.Name = "panelcut";
+            this.panelcut.Size = new System.Drawing.Size(450, 5);
+            this.panelcut.TabIndex = 6;
+            // 
+            // panelcolor
+            // 
+            this.panelcolor.BackColor = System.Drawing.SystemColors.Control;
+            this.panelcolor.Location = new System.Drawing.Point(426, 429);
+            this.panelcolor.Name = "panelcolor";
+            this.panelcolor.Size = new System.Drawing.Size(74, 20);
+            this.panelcolor.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(316, 429);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "สีของคำบรรยาย";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 429);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(78, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(74, 429);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ความสูงของคำบรรยาย";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(6, 16);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 395);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 385);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 55);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(511, 388);
+            this.axVLCPlugin21.TabIndex = 3;
+            // 
+            // picDataGrid
+            // 
+            this.picDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.picDataGrid.Location = new System.Drawing.Point(300, 449);
+            this.picDataGrid.Name = "picDataGrid";
+            this.picDataGrid.RowHeadersVisible = false;
+            this.picDataGrid.Size = new System.Drawing.Size(140, 168);
+            this.picDataGrid.TabIndex = 4;
+            this.picDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PicDataGrid_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1079, 619);
+            this.Controls.Add(this.picDataGrid);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "TranslateSub";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelcolor;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.DataGridView picDataGrid;
+        private System.Windows.Forms.Panel panelcut;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translatesub
+{
+    public partial class Form1 : Form
+    {
+        string nVDO = "";
+        Image img;
+        Bitmap bitmap,bmp;
+        Color color;
+        int value_color = 50;
+        int check = 0 ;
+        float sizeimg_H = 0 , sizeimg_W =0 ;
+        List<Bitmap> bitmaps = new List<Bitmap>();
+        string startupPath = Environment.CurrentDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+            var size = pictureBox1.Size;
+            trackBar1.Maximum = size.Height;
+            panelcut.Visible = false;
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                nVDO = openFileDialog1.SafeFileName;
+                txtInputFile.Text = openFileDialog1.SafeFileName;
+                txtInputFile.Visible = true;
+                Debug.WriteLine(openFileDialog1.FileName);
+
+                axVLCPlugin21.playlist.add(openFileDialog1.FileName, openFileDialog1.SafeFileName, null);
+                axVLCPlugin21.playlist.play();
+
+            }
+
+            if (!Directory.Exists(startupPath + "/" + nVDO))
+            {
+                Directory.CreateDirectory(startupPath + "/" + nVDO);
+            }
+            LaunchCommandLineApp(openFileDialog1.FileName, startupPath, nVDO);
+
+            
+
+            string[] files = Directory.GetFiles(startupPath + "/" + nVDO, "*.jpg");
+            DataTable table = new DataTable();
+            table.Columns.Add("File Name");
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = new FileInfo(files[i]);
+                table.Rows.Add(file.Name);
+                
+            }
+
+            picDataGrid.DataSource = table;
+            
+            
+        }
+        static void LaunchCommandLineApp(String nameVDO, String startupPath, String nVDO)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "ffmpeg.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = " -i \"" + nameVDO + "\" -vf fps=1 \"" + startupPath + "/" + nVDO + "\"/%04d.jpg";
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("error");
+            }
+
+        }
+        private void PicDataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string imgName = picDataGrid.CurrentRow.Cells[0].Value.ToString();
+            //รูปปกติ
+            img = Image.FromFile(startupPath + "/" + nVDO + "/" + imgName);
+            bitmap = (Bitmap)img;
+            var size = pictureBox1.Size;
+            //รูปที่รี
+            bitmap = ResizeImage(img, size.Width, size.Height);
+            pictureBox1.Image = bitmap;
+            
+
+        }
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return destImage;
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+            panelcut.Visible = true;
+            panelcut.Location = new Point(63, 399- trackBar1.Value);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            float reimg_h = (trackBar1.Value* 100) / bitmap.Height;
+            //ไซต์ image crob 
+            sizeimg_H = (img.Height * reimg_h) / 100;
+
+            Console.WriteLine("sum = "+ reimg_h + "     "+img.Height  + "   "+ sizeimg_H );
+
+            DirectoryInfo di = new DirectoryInfo(startupPath + "/" + nVDO);
+            FileInfo[] Images = di.GetFiles("*.jpg");
+            for (int i = 0; i < Images.Length; i++)
+            {
+
+                bmp = new Bitmap(Images[i].FullName);
+                Bitmap bmpCrop = bmp.Clone(new Rectangle(0, bmp.Height - (int)sizeimg_H, bmp.Width, (int)sizeimg_H), bmp.PixelFormat);
+                //list
+                bitmaps.Add(bmpCrop);
+                
+            }
+           
+        }      
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null && check == 0)
+            {
+                color = bitmap.GetPixel(e.X, e.Y);
+                panelcolor.BackColor = color;
+            }
+
+        }
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                color = bitmap.GetPixel(e.X, e.Y);
+                panelcolor.BackColor = color;
+                check = 1;
+            }
+
+        }
+
+        private void crop(Bitmap bmpcrop )
+        {
+
+            int a = color.A + value_color;
+            int r = color.R + value_color;
+            int g = color.G + value_color;
+            int b = color.B + value_color;
+
+            int a2 = color.A - value_color;
+            int r2 = color.R - value_color;
+            int g2 = color.G - value_color;
+            int b2 = color.B - value_color;
+            
+            
+            
+
+
+            /* for (int y = 0; y < bmpCrop.Height; y++)
+             {
+                 for (int x = 0; x < bmpCrop.Width; x++)
+                 {
+
+                     bmp_color = bmpCrop.GetPixel(x, y);
+                     int aa = bmp_color.A;
+                     int rr = bmp_color.R;
+                     int gg = bmp_color.G;
+                     int bb = bmp_color.B;
+                     if (rr >= r2 && rr <= r && gg >= g2 && gg <= g && bb >= b2 && bb <= b)
+                         bmpCrop.SetPixel(x, y, sub_color);
+                     else
+                         bmpCrop.SetPixel(x, y, back_color);
+
+                 }
+
+             }*/
+           // bmpCrop.Save(@"C:\Users\Anongrat\Desktop\output\" + imgName);
+
+           // pictureBox2.Image = bmpCrop;
+            
+        }
+    }
+}
